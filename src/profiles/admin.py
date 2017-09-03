bb73034dda5810cf4bec6132079893fbec62e6c1
Narongsak <narongsak.chobsri@gmail.com>
2017-09-03 22:33:09 +0700

    Create models profile

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import profile
 
 # Register your models here.
+class profileAdmin(admin.ModelAdmin):
+	class Meta:
+		model = profile
+
+admin.site.register(profile, profileAdmin)
