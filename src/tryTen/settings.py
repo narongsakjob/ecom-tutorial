8d8e1396648491eb70475b223915c668d6499458
Narongsak <narongsak.chobsri@gmail.com>
2017-09-05 09:42:00 +0700

    Can send email

--- src/tryTen/settings.py
+++ src/tryTen/settings.py
@@ -15,6 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'narongsak.chobsri@gmail.com'
+EMAIL_HOST_PASSWORD = 'Jobkub14773!'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles'
+    'profiles',
+    'contact'
 ]
 
 MIDDLEWARE = [
